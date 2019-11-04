c9fa39626f47af30920b873653d08981d2464cdd
kiki <772765729@qq.com>
2019-11-04 17:36:25 +0800

    ok

---
+++ build/precache-manifest.215926dd24a512cd62dad146585c5edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83bfa2a60efe422afda1acee23c2fb76",
+    "url": "/myapp/react-demo-2/build/index.html"
+  },
+  {
+    "revision": "2e9074b5b24855f59968",
+    "url": "/myapp/react-demo-2/build/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9030fe0ab8b8babe559e",
+    "url": "/myapp/react-demo-2/build/static/js/2.d1477d8f.chunk.js"
+  },
+  {
+    "revision": "2e9074b5b24855f59968",
+    "url": "/myapp/react-demo-2/build/static/js/main.f64f9f04.chunk.js"
+  },
+  {
+    "revision": "0b241d461ca6dddd14fd",
+    "url": "/myapp/react-demo-2/build/static/js/runtime-main.784cc238.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/react-demo-2/build/static/media/logo.25bf045c.svg"
+  }
+]);
